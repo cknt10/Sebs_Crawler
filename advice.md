9d5616c96c6114c40083271cd542dde5c2055cab
Lord_Gulasch <FlashPippin@gmx.net>
2022-05-19 00:03:09 +0200

    Co-authored-by: IetzBTR <IetzBTR@users.noreply.github.com>

--- advice.md
+++ advice.md
@@ -3,3 +3,9 @@ https://www.youtube.com/watch?v=5THvDt_sbfQ
 https://stackoverflow.com/questions/10227107/write-to-a-csv-in-node-js
 
 https://gulpjs.com/docs/en/getting-started/creating-tasks
+
+
+  //today.getFullYear()
+  //today.getMonth()+1
+  //today.getDate()
+  // var last = new Date(date.getTime() - (days * 24 * 60 * 60 * 1000));
--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,40 @@ import * as cheerio from "cheerio";
 import * as fs from "fs";
 import * as path from "path";
 import * as urlParser from "url";
+import * as util from 'util';
 
 const crawl = async (version: number) => {
 
-  const data = await fetch('https://partner.venuzle.de/citywave-regensburg/sharedbookings/1/bookableevents/ajax/?start=2022-05-09T00:00:00&end=2022-05-16T00:00:00')
-  .then((response) =>{return response.json()});
-
-  const result = data.map(value => {
-    return { start: value["start"], end: value["end"] }
-  });
-
-  const time = new Date (Date.now());
-
+  const days = 9;
+  const today = new Date();
+  const upcomingDays = new Date(today.getTime() + (days * 24 * 60 * 60 * 1000));
+  //'https://partner.venuzle.de/citywave-regensburg/sharedbookings/1/bookableevents/ajax/?start=2022-05-09T00:00:00&end=2022-05-16T00:00:00't
   try {
-    
-    fs.writeFile(`./dates/snippet_${ time.getDate() }-${ time.getMonth() }_v${ version }.txt`, JSON.stringify(result), (err) => {
+
+    const data = await fetch(`https://partner.venuzle.de/citywave-regensburg/sharedbookings/1/bookableevents/ajax/?start=${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}T00:00:00&end=${upcomingDays.getFullYear()}-${upcomingDays.getMonth()+1}-${upcomingDays.getDate()}T00:00:00`)
+    .then((response: any) =>{return response.json()});
+
+    const result = data.map((value: any) => {
+      console.log("value",value);
+      return {
+        id: value.id,
+        day: value.day,
+        from: value.from,
+        to: value.to,
+        css: value.css,
+        seats: value.seats,
+        customerSeats: value.customerSeats,
+        configured: value.configured,
+        bookable: value.bookable,
+        inPast: value.inPast,
+        text: value.text,
+        legendId: value.legendId,
+        legendText: value.legendText,
+        Timestamp: `${ today.getDate() }.${ today.getMonth()+1 } ${ today.getHours() }:${ today.getMinutes() }Uhr`
+        } //Need the following: id; day; from; to; css; seats; customerSeats; configured; bookable; inPast; text; legendId; legendText; CurrentTimeOfCrawler
+    });
+
+    fs.writeFile(`./dates/snippet.json`, JSON.stringify(result, null, 2), (err) => {
       if (err)
         console.log(err);
       else {
@@ -31,9 +50,12 @@ const crawl = async (version: number) => {
 
 /**/
 let i = 0;
-const repeater = setInterval(() => { 
+
+
+
+//const repeater = setInterval(() => { 
   crawl(i++);
- }, 5000);
+ //}, 5000);
 
 
 
